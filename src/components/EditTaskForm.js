ccf3a86c603391dff8cf162a3f302f3e696a6878
Jamal Kayed <119996570+Yuroq@users.noreply.github.com>
2023-07-24 15:35:16 -0400

    Uploaded project with Local-storage

--- src/components/EditTaskForm.js
+++ src/components/EditTaskForm.js
@@ -1,3 +1,27 @@
-function editTask() {
-    
+import { useState } from "react"
+
+function EditTask(props) {
+    const noteBeingEdited = props.editNote
+    const note = props.note
+    const [value,setValue] = useState(noteBeingEdited.note)
+    const [completeEdit,setCompleteEdit] = useState(false)
+    const [showForm,setShowForm] = useState(true)
+    function editSubmitHandler(e) {
+       
+e.preventDefault()
+note(value)
+setShowForm(false)
+setCompleteEdit(true)
+    }
+   return (
+    <div>
+  {showForm &&<form onSubmit={editSubmitHandler}>
+   <input style={{backgroundColor:"green"}}
+    value={value} onChange={(e) => setValue(e.target.value)}/>
+    </form>}
+{completeEdit && <h2>{value}</h2>}
+</div>
+   )
 }
+
+export default EditTask
